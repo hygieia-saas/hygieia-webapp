99dce06462d73ae76dced40f30cc24c3f0c3ef5e
Manuel Kießling <manuel@kiessling.net>
2022-02-19 15:13:05 +0100

    Styling

--- application/frontend/src/elements/ErrorMessage.tsx
+++ application/frontend/src/elements/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
+import FormElementError from './FormElementError';
 
 const ErrorMessage = ({ message }: { message: null | string }): JSX.Element => (
-    ((message !== null) && <div data-testid='errorMessageAlert'>{ message }</div>) || <Fragment />
+    ((message !== null) && <div className='max-w-xl'><FormElementError data-testid='errorMessageAlert'>{ message }</FormElementError></div>) || <Fragment />
 );
 
 export default ErrorMessage;
