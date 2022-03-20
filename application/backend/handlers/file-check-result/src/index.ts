3d7c7d8198b9ecba02f33b6a78e5cecb7d4b68cb
Manuel Kießling <manuel@kiessling.net>
2022-03-20 10:14:55 +0100

    WIP: file check result handler

--- application/backend/handlers/file-check-result/src/index.ts
+++ application/backend/handlers/file-check-result/src/index.ts
@@ -4,13 +4,13 @@ export const handler: S3Handler = async (event) => {
     console.debug('Received event', event);
 
     console.debug('stringify event');
-    JSON.stringify(event, null, 2);
+    console.debug(JSON.stringify(event, null, 2));
 
     console.debug('stringify event.Records[0].s3');
-    JSON.stringify(event.Records[0].s3, null, 2);
+    console.debug(JSON.stringify(event.Records[0].s3, null, 2));
 
     console.debug('stringify event.Records[0].requestParameters');
-    JSON.stringify(event.Records[0].requestParameters, null, 2);
+    console.debug(JSON.stringify(event.Records[0].requestParameters, null, 2));
 
     return;
 }
